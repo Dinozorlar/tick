baa7d2c9335667d76a769a8e5a151e70a8c6e063
Cankat Akdemir <cankatakdemir@gmail.com>
2017-03-30 23:24:39 +0300

    config files

---
+++ config/production.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'twig' => [
+        'twig.path' => APP_ROOT_DIR . '/src/Tick/views'
+    ],
+    'twig.folders' => [
+        [APP_ROOT_DIR . '/src/Tick/Web/App/Views', 'app']
+    ],
+    'app.debug' => false
+];
